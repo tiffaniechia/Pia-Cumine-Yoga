d9b26ddf5c4e956593b5f778a2aec338f5586a53
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-06-06 22:55:57 +0100

    updates favicons strategy

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
